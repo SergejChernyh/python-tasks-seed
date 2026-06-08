00b2ba157a7c8dbad01646e5e6f16445c5116b8f
Черных Сергей <st142136@student.spbu.ru>
2026-06-08 20:58:27 +0300

    Second homework

--- 02.Base85/base85_test.py
+++ 02.Base85/base85_test.py
@@ -1,9 +1,8 @@
 """
 Unit tests for 02.Base85
-
-TODO: Add more tests!
 """
 
+import pytest
 import base85ed
 
 
@@ -25,3 +24,29 @@ def test_shorts_decode():
     assert base85ed.decode(b"F){") == b"12"
     assert base85ed.decode(b"F)}j") == b"123"
     assert base85ed.decode(b"F)}kW") == b"1234"
+
+
+def test_longs_encode():
+    """
+    Test random long encodes
+    """
+    assert base85ed.decode(b"2c5uJ4m90Tvrm578H668LGyfTF") == b"11N1a8i1BF0N<49D)F7382`@c9LjA?<(6"
+    assert base85ed.decode(b"sr1vFDG432HnDHBuerCD42MJHR") == b"F)j!Y7RTa'1GVL=6qBXeATC.+1bqct85]"
+
+
+def test_longs_decode():
+    """
+    Test random long decodes
+    """
+    assert base85ed.decode(b"11N1a8i1BF0N<49D)F7382`@c9LjA?<(6") == b"2c5uJ4m90Tvrm578H668LGyfTF"
+    assert base85ed.decode(b"F)j!Y7RTa'1GVL=6qBXeATC.+1bqct85]") == b"sr1vFDG432HnDHBuerCD42MJHR"
+
+
+def test_overflow():
+    """
+    Test if the function raises an exceptions
+    """
+    with pytest.raises(ValueError):
+        base85ed.decode(b"~")
+    with pytest.raises(ValueError):
+        base85ed.decode(b"1234567890~")
--- 02.Base85/base85ed.py
+++ 02.Base85/base85ed.py
@@ -3,8 +3,13 @@ Base85 encoder and decoder
 """
 
 from __future__ import annotations
-import base64
 from beartype import beartype
+
+ENCODE_MAP = \
+    b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"
+DECODE_MAP = {char: idx for idx, char in enumerate(ENCODE_MAP)}
+
+powers85 = (52200625, 614125, 7225, 85, 1)
 
 
 @beartype
@@ -12,8 +17,16 @@ def encode(b: bytes):
     """
     Base85 encoder
     """
-    # TODO: IMPLEMENT !
-    return base64.b85encode(b, pad=False)
+    res = bytearray()
+    for i in range(0, len(b) - 3, 4):
+        val = int.from_bytes(b[i : i + 4])
+        res.extend([ENCODE_MAP[val // m % 85] for m in powers85])
+
+    if len(b) % 4:
+        val = int.from_bytes(b[len(b) - len(b) % 4 :] + b"\x00" * (4 - len(b) % 4))
+        res.extend([ENCODE_MAP[val // m % 85] for m in powers85][: len(b) % 4 + 1])
+
+    return bytes(res)
 
 
 @beartype
@@ -21,5 +34,21 @@ def decode(b: bytes):
     """
     Base85 decoder
     """
-    # TODO: IMPLEMENT !
-    return base64.b85decode(b)
+    res = bytearray()
+    if len(b) % 5 == 1:
+        raise ValueError("Incorrect length of the last chunk")
+
+    for i in range(0, len(b) - 4, 5):
+        val = sum(DECODE_MAP[b[i + j]] * powers85[j] for j in range(5))
+        if val >= 4294967296:
+            raise ValueError(f"base85 overflow in hunk starting at byte {i}")
+        res.extend(val.to_bytes(4))
+
+    if len(b) % 5:
+        last_chunk = b[len(b) - len(b) % 5 :] + b"~" * (5 - len(b) % 5)
+        val = sum(DECODE_MAP[last_chunk[j]] * powers85[j] for j in range(5))
+        if val >= 4294967296:
+            raise ValueError(f"base85 overflow in hunk starting at byte {len(b) - len(b) % 4}")
+        res.extend(val.to_bytes(4)[: len(b) % 5 - 1])
+
+    return bytes(res)
